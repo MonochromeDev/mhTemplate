20f3e919abee90d0107a7efa304b93c437a6643f
Evhenii <Holodniak.e@gmail.com>
2018-02-02 13:03:45 +0200

    add mh script and update success ajax script

--- app/gf/export.php
+++ app/gf/export.php
@@ -6,22 +6,25 @@ $response = array(); // сюда будем писать то что будем 
 
 $name = isset($_POST['name']) ? $_POST['name'] : false; // сунем каждое поле в отдельную переменную
 $email = isset($_POST['email']) ? $_POST['email'] : false;
-$mes = $_POST['mes'];
+$phone = isset($_POST['phone']) ? $_POST['phone'] : false;
+// $plan = $_POST['plan'];
+$utm_source = $_POST['utm_source'];
+$utm_medium = $_POST['utm_medium'];
+$utm_campaign = $_POST['utm_campaign'];
+$utm_term = $_POST['utm_term'];
 
-
-// сюда можно положить всякие проверки полей и капчу например
-if (!$name || !$email) { // в моем случае надо чтобы первые 2 поля не были пустыми
-    $response['ok'] = 0; // пишем что все плохо
-    $response['message'] = '<p class="error">Не заполнены первые два поля. Их всего два, ну ты чо? =)</p>'; // пишем ответ
-    die(json_encode($response)); //выводим массив в json формате и умираем
-}
 
 // теперь подготовим данные для отправки в гугл форму
-$url = 'https://docs.google.com/forms/d/e/1FAIpQLSfjZogNbahaSrDYrmVDH4GPiJYBcBiWErOAgGcCFdtKD0fB7w/formResponse'; // куда слать, это атрибут action у гугл формы
+$url = 'https://docs.google.com/forms/d/e/1FAIpQLScatpr29lsc3-qPjsDjT2_p6LfTVFw5Gc6tO2RgxNViFSPxuw/formResponse'; // куда слать, это атрибут action у гугл формы
 $data = array(); // массив для отправки в гугл форм
-$data['entry.465049200'] = $name; // указываем соответствия полей, ключи массива это нэймы оригинальных полей гуглформы
-$data['entry.1392616227'] = $email;
-$data['entry.1470590734'] = $mes;
+$data['entry.1405076119'] = $name;
+$data['entry.25521918'] = $email;
+$data['entry.882584547'] = $phone;
+// $data['entry.367410407'] = $plan;
+$data['entry.199889172'] = $utm_source;
+$data['entry.1881624589'] = $utm_medium;
+$data['entry.1252320012'] = $utm_campaign;
+$data['entry.432273211'] = $utm_term;
 
 
 $data = http_build_query($data); // теперь сериализуем массив данных в строку для отправки
@@ -45,6 +48,7 @@ if (!$result) { // если что-то не так
 
 $response['ok'] = 1; // если дошло до сюда, значит все ок
 $response['message'] = '<p class="">Все ок, отправилось.</p>'; // пишем ответ
+echo json_encode($data);
 die(json_encode($response)); //выводим массив в json формате и умираем
-
+   
 ?>
---
+++ app/mh/export.php
@@ -0,0 +1,53 @@
+<?php
+
+  $name = isset($_POST['name']) ? $_POST['name'] : false; // сунем каждое поле в отдельную переменную
+  $email = isset($_POST['email']) ? $_POST['email'] : false;
+  $phone = isset($_POST['phone']) ? $_POST['phone'] : false;
+  $utm_source = $_POST['utm_source'];
+  $utm_medium = $_POST['utm_medium'];
+  $utm_campaign = $_POST['utm_campaign'];
+  $utm_term = $_POST['utm_term'];
+  $product = $_POST['product'];
+  $referer = $_POST['referer'];
+  $ga_client_id = $_POST['ga_client_id'];
+  $country = $_POST['country'];
+  $plan = $_POST['plan'];
+  $sum = $_POST['sum'];
+  $sum_usd = $_POST['sum'];
+
+  $send_data = array();
+
+  $send_data['name'] = $name;
+  $send_data['email'] = $email;
+  $send_data['phone'] = $phone;
+  $send_data['product'] = $product;
+  $send_data['utm_source'] =  $utm_source;
+  $send_data['utm_campaign'] = $utm_campaign;
+  $send_data['utm_term'] = $utm_term;
+  $send_data['utm_medium'] = $utm_medium;
+  // $send_data['referer'] = $referer;
+  $send_data['plan'] = $plan;
+  $send_data['sum'] = $sum;
+  // $send_data['sum_usd'] = $sum_usd;
+  $send_data['ga_client_id'] = $ga_client_id;
+  $send_data['country'] = $country;
+
+
+
+  // $send_data['additional'] = 'no_amo';
+
+ if( $curl = curl_init() ) {
+    curl_setopt($curl, CURLOPT_URL, 'https://paysaleshub.monochrome.me/lead_magnet_2');
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER,true);
+    curl_setopt($curl, CURLOPT_POST, true);
+    curl_setopt($curl, CURLOPT_POSTFIELDS, http_build_query($send_data));
+    $out = curl_exec($curl);
+    echo $out;
+    curl_close($curl);
+  }
+  $output = array($send_data);
+  echo json_encode($output);
+
+
+
+?>
